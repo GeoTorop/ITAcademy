c05a551cc7c15781682533ceaf728958bef3f2f2
George Torop <gotorop92@gmail.com>
2023-05-13 23:51:43 +0300

    Fixed CustomException

--- src/com/epam/task/array/exceptions/CustomException.java
+++ src/com/epam/task/array/exceptions/CustomException.java
@@ -1,7 +1,19 @@
 package com.epam.task.array.exceptions;
 
 public class CustomException extends Exception {
+    public CustomException() {
+
+    }
+
     public CustomException(String message) {
         super(message);
+    }
+
+    public CustomException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CustomException(Throwable cause) {
+        super(cause);
     }
 }
