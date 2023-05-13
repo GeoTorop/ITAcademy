2a8e43f9ede9a38ce195627e4301c58899d56b9b
George Torop <gotorop92@gmail.com>
2023-05-13 23:52:14 +0300

    Fixed bug

--- src/com/epam/task/array/main/Main.java
+++ src/com/epam/task/array/main/Main.java
@@ -1,6 +1,5 @@
 package com.epam.task.array.main;
 
-import com.epam.task.array.reader.CustomArrayReader;
 import com.epam.task.array.entity.CustomArray;
 import com.epam.task.array.services.ArrayCalculationServiceInterface;
 import com.epam.task.array.services.ArraySortServiceInterface;
@@ -10,13 +9,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Main {
     private static final Logger LOGGER = LogManager.getLogger();
     private static final int[] ARRAY_OF_NUM = {0, 2, 10, 20, 70, 1000, 0, -5, -6, 3};
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         CustomArray customArray = new CustomArray(ARRAY_OF_NUM);
         ArrayCalculationServiceInterface arrayCalculationServiceInterface = new ArrayCalculationServiceInterfaceImpl();
         ArraySortServiceInterface arraySortServiceInterface = new ArraySortServiceInterfaceImpl();
@@ -32,10 +30,5 @@ public class Main {
         arraySortServiceInterface.bubbleSort(customArray);
         arraySortServiceInterface.selectionSort(customArray);
         arraySortServiceInterface.insertionSort(customArray);
-
-        CustomArrayReader reader = new CustomArrayReader("data/array.txt");
-        int[] array = reader.readArray();
-        int min = arrayCalculationServiceInterface.findMin(customArray);
-        System.out.println(Arrays.toString(array));
     }
 }
