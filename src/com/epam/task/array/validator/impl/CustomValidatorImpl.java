aa7e74d52f352692e56d3bcad3397efae463797b
George Torop <gotorop92@gmail.com>
2023-05-16 19:08:21 +0300

    Changed name of method

--- src/com/epam/task/array/validator/impl/CustomValidatorImpl.java
+++ src/com/epam/task/array/validator/impl/CustomValidatorImpl.java
@@ -1,11 +1,11 @@
 package com.epam.task.array.validator.impl;
 
-import com.epam.task.array.validator.CustomValidatorInterface;
+import com.epam.task.array.validator.CustomValidator;
 
-public class CustomValidatorImpl implements CustomValidatorInterface {
+public class CustomValidatorImpl implements CustomValidator {
     private static final String INT_ARRAY_REGEX = "([0-9]{1,3}\\s+)+(\\d{1,3})";
 
-    public boolean stringArrayValidation(String stringArray) {
+    public boolean stringArrayValidate(String stringArray) {
         boolean match = stringArray.matches(INT_ARRAY_REGEX);
         return match;
     }
