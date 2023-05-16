840052e7f1cef2cab70922df581d92828eeecbff
George Torop <gotorop92@gmail.com>
2023-05-16 19:01:35 +0300

    Added REGEX_DELIMM

--- src/com/epam/task/array/customreader/impl/CustomArrayReaderImpl.java
+++ src/com/epam/task/array/customreader/impl/CustomArrayReaderImpl.java
@@ -1,8 +1,8 @@
 package com.epam.task.array.customreader.impl;
 
-import com.epam.task.array.customreader.CustomArrayReaderInterface;
+import com.epam.task.array.customreader.CustomArrayReader;
 import com.epam.task.array.exceptions.CustomException;
-import com.epam.task.array.validator.CustomValidatorInterface;
+import com.epam.task.array.validator.CustomValidator;
 import com.epam.task.array.validator.impl.CustomValidatorImpl;
 
 import java.io.IOException;
@@ -11,7 +11,8 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CustomArrayReaderImpl implements CustomArrayReaderInterface {
+public class CustomArrayReaderImpl implements CustomArrayReader {
+    private static final String REGEX_DELIMM = "\\s+";
     public List<int[]> readCustomArray(String filename) throws CustomException {
         Path path = Path.of(filename);
         if (!Files.exists(path)) {
@@ -22,11 +23,11 @@ public class CustomArrayReaderImpl implements CustomArrayReaderInterface {
         try {
             List<String> lines = Files.readAllLines(path);
             System.out.println(lines);
-            CustomValidatorInterface validator = new CustomValidatorImpl();
+            CustomValidator validator = new CustomValidatorImpl();
             for (int i = 0; i < lines.size(); i++) {
                 String stringOfNumbers = lines.get(i);
-                if(validator.stringArrayValidation(stringOfNumbers)){
-                    String[] numberOfArrays = stringOfNumbers.split("\\s+");
+                if(validator.stringArrayValidate(stringOfNumbers)){
+                    String[] numberOfArrays = stringOfNumbers.split(REGEX_DELIMM);
                     oneArray = new int[numberOfArrays.length];
                     for (int j = 0; j < numberOfArrays.length; j++) {
                         oneArray[j] = Integer.parseInt(numberOfArrays[j]);
